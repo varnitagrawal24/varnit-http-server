32b1f638348114347fc1a949ad5269a291a6dc45
Varnit Agrawal <varnit.agrawal24@gmail.com>
2023-09-11 10:57:17 +0530

    add /html response

---
+++ index.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("<h1>Hello</h1>");
+  } else if (req.url === "/html") {
+    fs.readFile("index.html", (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.end(data);
+      }
+    });
+  } else {
+    res.end("<h1>Page not found</h1>");
+  }
+});
+
+server.listen(3000, () => {
+  console.log("server is working....!!!");
+});
