847ddf8dfd8aee642f4f9d83c2e96bfc5d91be93
Varnit Agrawal <varnit.agrawal24@gmail.com>
2023-09-13 15:18:59 +0530

    add /uuid response

--- index.js
+++ index.js
@@ -17,6 +17,11 @@ app.get('/json',(req,res)=>{
   res.sendFile(path.join(__dirname,"index.json"));
 })
 
+app.get('/uuid',(req,res)=>{
+  const uuid=uuidv4()
+  res.json({uuid:uuid})
+})
+
 app.listen(3000, () => {
   console.log("server is live on 3000....!!!");
 });
